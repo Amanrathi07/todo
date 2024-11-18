0ff2a6d70975e311a9edceadc0749ae45091ab99
Amanrathi07 <amanrathi4321@gmail.com>
2024-11-18 12:24:49 -0800

    add title and disc

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const[allTodos,setAllTodos]=useState([])
+  const [singleTodo,setSingleTodo] = useState({title:"",disc:""})
+  
+
+  function hendelAddTodo() {
+    console.log(singleTodo);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+        <input type="text" placeholder="title" onChange={(e)=> {setSingleTodo((prevValue)=>({...prevValue,title:e.target.value}))}} />
+            <br />
+            <br />  
+        <input type="text" placeholder='dics'  onChange={(e)=> {setSingleTodo((prevValue)=>({...prevValue,disc:e.target.value}))}} />
+            <br />
+            <br />
+        <button onClick={hendelAddTodo}>add todo</button>
+    </div>   
+
+
+
     </>
   )
 }
